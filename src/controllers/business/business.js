81bfb26fff6f1ee4c57f92697ba2103d395292dd
Nilesh kumar <162776615+NileshKumar2404@users.noreply.github.com>
2025-09-19 10:23:41 +0530

    Refactor ownership check in updateMyBusiness to ensure proper type comparison

--- src/controllers/business/business.js
+++ src/controllers/business/business.js
@@ -64,7 +64,6 @@ export const updateMyBusiness = asyncHandler(async (req, res) => {
             return res.status(403).json(new ApiResponse(403, {}, "Only shop owner can update businesses"));
         }
 
-
         // console.log(`This is our business id ${typeof business.ownerId} and this is our logged in user id ${typeof req.user._id}`);
         
         if (req.user._id.toString() !== business.ownerId.toString()) {
