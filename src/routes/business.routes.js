d315d75a3d59ad6da477d9eb45413764dde837a4
Codewhatyouwant <mukeshkumhar906@gmail.com>
2025-10-12 21:11:20 +0530

    Working Whatsapp

--- src/routes/business.routes.js
+++ src/routes/business.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyUser } from '../middleware/authMiddleware.js'
-import { createBusiness, deleteMyBusiness, getMyBusiness, listBusinesses_admin, transferOwnerShip_admin, updateMyBusiness, updateWAConfigMyBusiness } from '../controllers/business/business.js'
+import { createBusiness, deleteMyBusiness, getMyBusiness, listBusinesses_admin, transferOwnerShip_admin, updateMyBusiness, getAllMyBusinesses, updateBusinessById } from '../controllers/business/business.js'
 
 const router = express.Router()
 
@@ -23,8 +23,7 @@ router.route("/create-business").post(verifyUser, createBusiness)
 router.route("/get-my-business").get(verifyUser, getMyBusiness)
 router.route("/update-business/:businessId").patch(verifyUser, updateMyBusiness)
 router.route("/delete-business/:businessId").delete(verifyUser, deleteMyBusiness)
-router.route("/list-business").get(verifyUser, )
+router.route("/list-business").get(verifyUser, listBusinesses_admin)
 router.route("/transfer-ownership/:businessId").get(verifyUser, transferOwnerShip_admin)
-router.route("/:businessId/wa-confi").patch(verifyUser,updateWAConfigMyBusiness)
 
 export default router
--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,16 +1,10 @@
 import express from "express";
-import { addCustomer, importCustomers, listCustomers } from "../controllers/customer.controllers.js";
+import { addCustomer, listCustomers, updateCustomer, deleteCustomer } from "../controllers/customer.controllers.js";
 import { verifyUser } from "../middleware/authMiddleware.js";
-import { parseSingleFile, upload } from "../middleware/upload.middleware.js";
 
 
 export const customerRouter = express.Router();
-
 customerRouter.post("/", verifyUser, addCustomer);
 customerRouter.get("/", verifyUser, listCustomers);
-customerRouter.post(
-    "/upload",
-    verifyUser,
-    parseSingleFile,          // form-data key: file
-    importCustomers
-);
+customerRouter.put("/:id", verifyUser, updateCustomer);
+customerRouter.delete("/:id", verifyUser, deleteCustomer);
