a6e986a32104c7833d2ec3bfb6b1ec40793f384a
Pankaj Kumar Agarwal <pankaj.7613@gmail.com>
2025-10-12 09:50:54 +0530

    fix

--- src/models/index.js
+++ src/models/index.js
@@ -4,16 +4,14 @@ import { Customer } from './Customer.js';
 import { Template } from './Template.js';
 import { Campaign } from './Campaign.js';
 import { MessageLog } from './MessageLog.js';
+import Employee from './Employee.js';
 
 // Define associations
-User.hasMany(Business, { foreignKey: 'ownerId', as: 'businesses' });
-Business.belongsTo(User, { foreignKey: 'ownerId', as: 'owner' });
+User.hasMany(Business, { foreignKey: 'userId', as: 'businesses' });
+Business.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
 User.hasMany(Customer, { foreignKey: 'userId', as: 'customers' });
 Customer.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-Business.hasMany(Customer, { foreignKey: 'businessId', as: 'customers' });
-Customer.belongsTo(Business, { foreignKey: 'businessId', as: 'business' });
 
 User.hasMany(Template, { foreignKey: 'userId', as: 'templates' });
 Template.belongsTo(User, { foreignKey: 'userId', as: 'user' });
@@ -21,11 +19,11 @@ Template.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 User.hasMany(Campaign, { foreignKey: 'userId', as: 'campaigns' });
 Campaign.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
+User.hasMany(Employee, { foreignKey: 'userId', as: 'employees' });
+Employee.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 Template.hasMany(Campaign, { foreignKey: 'templateId', as: 'campaigns' });
 Campaign.belongsTo(Template, { foreignKey: 'templateId', as: 'template' });
-
-Business.hasMany(Campaign, { foreignKey: 'businessId', as: 'campaigns' });
-Campaign.belongsTo(Business, { foreignKey: 'businessId', as: 'business' });
 
 Campaign.hasMany(MessageLog, { foreignKey: 'campaignId', as: 'messageLogs' });
 MessageLog.belongsTo(Campaign, { foreignKey: 'campaignId', as: 'campaign' });
@@ -33,11 +31,15 @@ MessageLog.belongsTo(Campaign, { foreignKey: 'campaignId', as: 'campaign' });
 Customer.hasMany(MessageLog, { foreignKey: 'customerId', as: 'messageLogs' });
 MessageLog.belongsTo(Customer, { foreignKey: 'customerId', as: 'customer' });
 
+Template.hasMany(MessageLog, { foreignKey: 'templateId', as: 'messageLogs' });
+MessageLog.belongsTo(Template, { foreignKey: 'templateId', as: 'template' });
+
 export {
   User,
   Business,
   Customer,
   Template,
   Campaign,
-  MessageLog
+  MessageLog,
+  Employee
 };
