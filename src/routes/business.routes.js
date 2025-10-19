5f10aa9a83a4614bbce4e93878ad1655934eb3b4
Codewhatyouwant <mukeshkumhar906@gmail.com>
2025-10-19 21:45:58 +0530

    pull from pankaj fixed

--- src/routes/business.routes.js
+++ src/routes/business.routes.js
@@ -23,8 +23,7 @@ router.route("/create-business").post(verifyUser, createBusiness)
 router.route("/get-my-business").get(verifyUser, getMyBusiness)
 router.route("/update-business/:businessId").patch(verifyUser, updateMyBusiness)
 router.route("/delete-business/:businessId").delete(verifyUser, deleteMyBusiness)
-router.route("/list-business").get(verifyUser, )
+router.route("/list-business").get(verifyUser, listBusinesses_admin)
 router.route("/transfer-ownership/:businessId").get(verifyUser, transferOwnerShip_admin)
-router.route("/:businessId/wa-confi").patch(verifyUser,updateWAConfigMyBusiness)
 
 export default router
